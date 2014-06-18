072af384deb81ae7ab29f44006de7186afed701a
Ionică Bizău <bizauionica@yahoo.com>
2014-06-18 09:18:14 +0300

    Added test page

---
+++ test/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var e = new MediumEditor(".container", {
+        buttons: ["bold", "italic", "underline", "anchor", "header1", "header2", "quote"],
+        extensions: {
+        }
+    });
+};
