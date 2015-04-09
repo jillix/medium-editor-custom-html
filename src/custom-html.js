eff98150a6a374344333e783ecc608f23ae1e790
Ariel Rodriguez <arielsvn@gmail.com>
2015-04-08 21:56:34 -0400

    Moved the insertHtmlAtCaret function outside of the constructor
    Refactor insertHtmlAtCaret as helper instead outside the constructor of CustomHtml

--- src/custom-html.js
+++ src/custom-html.js
@@ -1,4 +1,38 @@
 (function () {
+    function insertHtmlAtCaret(html) {
+        var sel, range;
+        if (window.getSelection) {
+            // IE9 and non-IE
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+
+                // Range.createContextualFragment() would be useful here but is
+                // only relatively recently standardized and is not supported in
+                // some browsers (IE9, for one)
+                var el = document.createElement("div");
+                el.innerHTML = html;
+                var frag = document.createDocumentFragment(), node, lastNode;
+                while ((node = el.firstChild)) {
+                    lastNode = frag.appendChild(node);
+                }
+                range.insertNode(frag);
+
+                // Preserve the selection
+                if (lastNode) {
+                    range = range.cloneRange();
+                    range.setStartAfter(lastNode);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            }
+        } else if (document.selection && document.selection.type != "Control") {
+            // IE < 9
+            document.selection.createRange().pasteHTML(html);
+        }
+    }
 
     /**
      * CustomHtml
@@ -24,40 +58,6 @@
         }
         this.button.onclick = this.onClick.bind(this);
         this.options = options;
-        this.insertHtmlAtCaret = function (html) {
-            var sel, range;
-            if (window.getSelection) {
-                // IE9 and non-IE
-                sel = window.getSelection();
-                if (sel.getRangeAt && sel.rangeCount) {
-                    range = sel.getRangeAt(0);
-                    range.deleteContents();
-
-                    // Range.createContextualFragment() would be useful here but is
-                    // only relatively recently standardized and is not supported in
-                    // some browsers (IE9, for one)
-                    var el = document.createElement("div");
-                    el.innerHTML = html;
-                    var frag = document.createDocumentFragment(), node, lastNode;
-                    while ((node = el.firstChild)) {
-                        lastNode = frag.appendChild(node);
-                    }
-                    range.insertNode(frag);
-
-                    // Preserve the selection
-                    if (lastNode) {
-                        range = range.cloneRange();
-                        range.setStartAfter(lastNode);
-                        range.collapse(true);
-                        sel.removeAllRanges();
-                        sel.addRange(range);
-                    }
-                }
-            } else if (document.selection && document.selection.type != "Control") {
-                // IE < 9
-                document.selection.createRange().pasteHTML(html);
-            }
-        }
     }
 
     /**
@@ -68,7 +68,7 @@
      * @function
      */
     CustomHtml.prototype.onClick = function () {
-        this.insertHtmlAtCaret(this.options.htmlToInsert);
+        insertHtmlAtCaret(this.options.htmlToInsert);
     };
 
     /**
